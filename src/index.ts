5e628087a78753ec619e2b3c2eaa6f7ae8c4891d
Ratovoson81 <Ratovoson81@gmail.com>
2021-06-10 15:54:09 +0300

    connect mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import { json } from "body-parser";
 import express from "express";
 import { routerCar } from "./routes/car";
+import mongoose from "mongoose";
+import http from "http";
 
 const app = express();
 app.use(json());
 
+const server = http.createServer(app);
+
+mongoose
+  .connect(
+    "mongodb+srv://test:test@cluster0.cp0og.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((error) => console.log(error));
+
 app.use(routerCar);
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("serveur start on port 3000");
 });
