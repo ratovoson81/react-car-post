34e1fd974da63d15e6d6c0a224f24b2798411771
Anthony <Ratovoson81@gmail.com>
2021-06-13 13:22:19 +0300

    refact

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./pages/App";
 import "./css/tailwind.css";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store } from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
