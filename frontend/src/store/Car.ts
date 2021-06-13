d16e765830bbeb88b1fb6421f479dd955371a323
Anthony <Ratovoson81@gmail.com>
2021-06-13 13:19:11 +0300

    init store

---
+++ frontend/src/store/Car.ts
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "./store";
+
+// Define a type for the slice state
+interface CarState {
+  cars: [];
+}
+
+// Define the initial state using that type
+const initialState: CarState = {
+  cars: [],
+};
+
+export const carSlice = createSlice({
+  name: "car",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    increment: (state) => {
+      //state.car += 1;
+    },
+    decrement: (state) => {
+      //state.car -= 1;
+    },
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      //state.car += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = carSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectCar = (state: RootState) => state.car.cars;
+
+export default carSlice.reducer;
---
+++ frontend/src/store/Store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import carReducer from "./Car";
+
+export const store = configureStore({
+  reducer: {
+    car: carReducer,
+  },
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
